f12e0f3c0a29cf5e81a9ba90d4cbe6583356f4ca
Christopher Weir <christopher_weir@me.com>
2015-07-10 13:40:40 +0100

    Added scroll in video

---
+++ angular-scrollinVideo.js
@@ -0,0 +1,35 @@
+/**
+ * Angular scrollin
+ * An angularjs directive to add a class as the user scrolls down the page
+ *
+ *
+ */
+angular.module('angular-scrollin-video', [])
+    .directive('ilnScrollInVideo', [
+        '$window',
+        function ( $window ) {
+        return {
+            restrict: 'A',
+            link:   function ( scope, elm, attrs ) {
+
+                var hasPlayed               = false;
+
+                var elementPositionTop      = elm[0].getBoundingClientRect().top;
+                var elementPositionOffset   = attrs.scrollInOffset || 0.75;
+                var elementPositionClass    = attrs.scrollInClass || 'active';
+
+                // bind scroll listner to the window
+                angular.element($window).bind('scroll', function () {
+
+                    elementPositionTop      = elm[0].getBoundingClientRect().top;
+
+                    if( elementPositionTop <= $window.innerHeight * elementPositionOffset ){
+                        elm.addClass( elementPositionClass );
+                    }
+
+                    scope.$apply();
+                });
+
+            }
+        };
+    }]);
